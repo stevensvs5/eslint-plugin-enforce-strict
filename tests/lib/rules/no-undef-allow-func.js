32c41272ab770c0de27aa2b7d6b2e8903d44ba8a
Steven Santos <stevens@imstransport.com>
2019-08-06 17:19:58 -0400

    added allow typeof condition

--- tests/lib/rules/no-undef-allow-func.js
+++ tests/lib/rules/no-undef-allow-func.js
@@ -27,7 +27,8 @@ ruleTester.run("no-undef-allow-func", rule, {
     valid: [
         // give me some code that won't trigger a warning
         { code: "var foo = 3;" },
-        { code: "reports_alert();" }
+        { code: "reports_alert();" },
+        { code: "typeof test;" }
     ],
 
     invalid: [
@@ -35,6 +36,27 @@ ruleTester.run("no-undef-allow-func", rule, {
             code: "foo = 3;",
             errors: [{
                 message: "'foo' is not defined.",
+                type: "Identifier"
+            }]
+        },
+        {
+            code: "var foo = 2; \nbar = 3;",
+            errors: [{
+                message: "'bar' is not defined.",
+                type: "Identifier"
+            }]
+        },
+        {
+            code: "var foo = 2; bar = 3;",
+            errors: [{
+                message: "'bar' is not defined.",
+                type: "Identifier"
+            }]
+        },
+        {
+            code: "test;",
+            errors: [{
+                message: "'test' is not defined.",
                 type: "Identifier"
             }]
         }
